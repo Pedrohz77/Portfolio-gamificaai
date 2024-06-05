9743d9c3306072b71f7d276d7f2fbca5f9f2441c
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-06-05 14:23:46 -0300

    feat: Portfolio iniciado

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { Engine, Fade, FadeInOut } from "excalibur";
+import { welcomeScene } from "./scenes/welcomeScene";
+import { loader } from "./resources";
+import { historyScene } from "./scenes/historyScene";
+
+const game = new Engine({
+  width: 1200,
+  height: 800,
+  canvasElementId: "jogo"
+})
+
+game.addScene("bemvindo", new welcomeScene())
+
+game.addScene("historia", new historyScene())
+
+game.start(loader).then(() => {
+  game.goToScene("bemvindo", {
+    sourceOut: new FadeInOut({ duration: 1000})
+  })
+})
